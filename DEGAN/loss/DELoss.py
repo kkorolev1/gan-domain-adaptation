87cbb1f011be9b17e82af23dab56787bf5d5e7bf
Kirill Korolev <kakorolev@edu.hse.ru>
2024-04-05 00:13:13 +0300

    direction loss refactoring

--- DEGAN/loss/DELoss.py
+++ DEGAN/loss/DELoss.py
@@ -4,8 +4,18 @@ import torch.nn.functional as F
 
 
 class DELoss(nn.Module):
-    def __init__(self):
+    def __init__(self, mult_direction=1):
         super().__init__()
+        self.mult_direction = mult_direction
 
-    def forward(self, domain_dir_pred, domain_dir):
-        return 1 - torch.einsum("ij,ij->i", domain_dir_pred, domain_dir).mean()
+    def forward(self, gen_emb, src_emb, domain_emb, src_emb_mc):
+        domain_dir_pred = gen_emb - src_emb
+        domain_dir_pred_norm = torch.norm(domain_dir_pred)
+
+        domain_dir = domain_emb - src_emb_mc
+        domain_dir_norm = torch.norm(domain_dir)
+
+        cos_sim = torch.einsum("ij,ij->i", domain_dir_pred, domain_dir) / domain_dir_pred_norm / domain_dir_norm
+
+        loss_direction = 1 - cos_sim.mean()
+        return self.mult_direction * loss_direction
