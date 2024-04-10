4649f11ebfa20dc076d3fcd77c7aab83389da81c
Kirill Korolev <kakorolev@edu.hse.ru>
2024-04-10 22:18:14 +0300

    eval of blendgan and styledomain

---
+++ auxiliary/scripts/evaluate_blendgan.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+
+import cv2
+import numpy as np
+import torch
+import hydra
+from omegaconf import OmegaConf
+
+from model import Generator
+from psp_encoder.psp_encoders import PSPEncoder
+from utils import ten2cv, cv2ten
+import glob
+import random
+
+seed = 0
+
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
+@hydra.main(version_base=None, config_path="../../configs", config_name="blendgan")
+def main(config):
+    OmegaConf.resolve(config)
+    config = OmegaConf.to_container(config)
+
+    device = 'cuda'
+
+    g_config = config["generator"]
+
+    checkpoint = torch.load(g_config["ckpt"])
+    model_dict = checkpoint['g_ema']
+    print('ckpt: ', g_config["ckpt"])
+
+    g_ema = Generator(
+        g_config["size"], g_config["style_dim"], g_config["n_mlp"], channel_multiplier=g_config["channel_multiplier"], load_pretrained_vgg=False
+    ).to(device)
+    g_ema.load_state_dict(model_dict)
+    g_ema.eval()
+
+    psp_encoder = PSPEncoder(config["encoder"]["ckpt"], output_size=g_config["size"]).to(device)
+    psp_encoder.eval()
+
+    input_img_paths = sorted(glob.glob(os.path.join(config["input_img_path"], '*.*')))
+    style_img_paths = sorted(glob.glob(os.path.join(config["style_img_path"], '*.*')))[:]
+
+    num = 0
+
+    for input_img_path in input_img_paths:
+        print(num)
+        num += 1
+
+        name_in = os.path.splitext(os.path.basename(input_img_path))[0]
+        img_in = cv2.imread(input_img_path, 1)
+        img_in_ten = cv2ten(img_in, device)
+        img_in = cv2.resize(img_in, (g_config["size"], g_config["size"]))
+
+        for style_img_path in style_img_paths:
+            name_style = os.path.splitext(os.path.basename(style_img_path))[0]
+            img_style = cv2.imread(style_img_path, 1)
+            img_style_ten = cv2ten(img_style, device)
+            img_style = cv2.resize(img_style, (g_config["size"], g_config["size"]))
+
+            with torch.no_grad():
+                sample_style = g_ema.get_z_embed(img_style_ten)
+                sample_in = psp_encoder(img_in_ten)
+                img_out_ten, _ = g_ema([sample_in], z_embed=sample_style, add_weight_index=6,
+                                       input_is_latent=True, return_latents=False, randomize_noise=False)
+                img_out = ten2cv(img_out_ten)
+            out = np.concatenate([img_in, img_style, img_out], axis=1)
+            # out = img_out
+            save_dir = os.path.join(config["save_dir"], name_style)
+            os.makedirs(save_dir, exist_ok=True)
+            cv2.imwrite(os.path.join(save_dir, f'{name_in}.png'), out)
+
+    print('Done!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ auxiliary/scripts/evaluate_styledomain.py
@@ -0,0 +1,100 @@
+import os
+import torch
+import PIL
+from tqdm import trange, tqdm
+from argparse import Namespace
+
+from torchvision.transforms import Resize
+from omegaconf import OmegaConf
+import hydra
+from core.utils.example_utils import (
+    Inferencer, to_im
+)
+from restyle_encoders.e4e import e4e
+from core.utils.reading_weights import read_weights
+
+from pathlib import Path
+from collections import defaultdict
+
+from examples.draw_util import weights, IdentityEditor, StyleEditor
+from degan.datasets import FFHQDataset
+
+
+def get_concat_h(im1, im2):
+    dst = PIL.Image.new('RGB', (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
+
+
+@hydra.main(version_base=None, config_path="../../configs", config_name="styledomain")
+def main(config):
+    OmegaConf.resolve(config)
+    config = OmegaConf.to_container(config)
+
+    device = 'cuda'
+    dataset = FFHQDataset(config["ffhq_path"], 256)
+    print(f"Dataset size {len(dataset)}")
+
+    model_path = config['encoder']['ckpt']
+    ckpt = torch.load(model_path, map_location='cuda:0')
+    opts = ckpt['opts']
+    opts['checkpoint_path'] = model_path
+    opts = Namespace(**opts)
+    net = e4e(opts).eval().to(device)
+
+    w_singles = []
+
+    with torch.no_grad():
+        for i in trange(len(dataset)):
+            img = dataset[i].unsqueeze(0).to(device)
+            _, w_plus = net(img, randomize_noise=False, return_latents=True)
+            w_singles.append(w_plus)
+
+    row_domains = config["eval_targets"]
+    style_to_editor = {
+        d: StyleEditor(read_weights(weights[d])) if d != 'original' else IdentityEditor() for d in row_domains
+    }
+
+    offset_pow = 0.85
+
+    dom_to_pow = defaultdict(lambda : offset_pow, {
+        'original': 0.,
+        'sketch': 0.7,
+        'anastasia': 0.7,
+        'pixar': 0.75,
+        'botero': 0.75,
+        'joker': 0.65,
+        'edvard_munch_painting': 0.95,
+        'modigliani_painting': 0.75
+    })
+
+    ckpt = read_weights(weights["anastasia"])
+    ckpt_ffhq = {'sg2_params': ckpt['sg2_params']}
+    ckpt_ffhq['sg2_params']['checkpoint_path'] = weights["ffhq"]
+    model = Inferencer(ckpt_ffhq, device).to(device)
+    resize = Resize(1024)
+
+    s_singles = [model.sg2_source.get_s_code([w_single], input_is_latent=True) for w_single in w_singles]
+
+    for row_domain in row_domains:
+        im_style = PIL.Image.open(os.path.join(config["domains_dir"], f"{row_domain}.png"))
+        save_dir = os.path.join(config["save_dir"], row_domain)
+        os.makedirs(save_dir, exist_ok=True)
+        print(f"Processing {row_domain}")
+        for i, s_single in enumerate(tqdm(s_singles)):
+            model.sg2_source.generator.load_state_dict(torch.load(weights["ffhq"])['g_ema'])
+            s_edited = style_to_editor[row_domain](s_single, power=dom_to_pow[row_domain])
+            src_im, _ = model.sg2_source(
+                s_edited, is_s_code=True
+            )
+            im_stylized = to_im(resize(src_im))
+            src_im, _ = model.sg2_source(
+                s_single, is_s_code=True
+            )
+            im_source = to_im(resize(src_im))
+            im = get_concat_h(get_concat_h(im_source, im_style), im_stylized)
+            im.save(os.path.join(save_dir, f"{i+1}.png"))
+
+if __name__ == '__main__':
+    main()
